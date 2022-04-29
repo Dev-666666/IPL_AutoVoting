14e5c22866d658eb31d3f9acd0b6fb600226680f
Admin <Admin@GURUSHRI>
2022-04-29 12:54:12 +0530

    Done

--- VSCode/IPLVotingSystem/Areas/User/Controllers/UserController.cs
+++ VSCode/IPLVotingSystem/Areas/User/Controllers/UserController.cs
@@ -3,20 +3,51 @@ using System.Collections.Generic;
 using System;
 using System.Threading.Tasks;
 using System.Linq;
+using IPLVotingSystem.Models;
+using IPLVotingSystem.Data;
+using Microsoft.AspNetCore.Http;
 
 namespace IPLVotingSystem.Areas.User.Controllers
 {
     [Area("User")]
     public class UserController : Controller
     {
+        ApplicationDbContext _db;
+
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
-        public IActionResult ViewQuestions()
+        public IActionResult ViewQuestions(IFormCollection form)
         {
-
+            Options option = new Options();
+            Questionary quest = new Questionary();
+            var resultq = _db.questionaries.OrderBy(x => x.qId).Take(1).SingleOrDefault();
+            option.qId = resultq.qId;
+            var result = _db.options.OrderBy(x => x.optionId).Take(1).SingleOrDefault();
+            var opid = result.optionId;
+            var res = _db.questionaries.ToList();
+            var reso = _db.options.ToList();
+            ViewBag.showquestion = resultq.Question;
+            ViewBag.showq = res;
+            ViewBag.showo = reso;
+             ViewBag.op1 = result.option1;
+             ViewBag.op2 = result.option2;
+            ViewBag.op3 = result.option3;
+            ViewBag.op4 = result.option4;
+           /* var shifts = (from b in _db.questionaries
+                          select new Questionary
+                          {
+                              Question = (string)b.Question
+                          }).ToList();
+            ViewBag.shift = shifts;*/
             return View();
+
+           
         }
         public IActionResult Feedback()
         {
--- VSCode/IPLVotingSystem/Areas/admin/Controllers/AdminUseController.cs
+++ VSCode/IPLVotingSystem/Areas/admin/Controllers/AdminUseController.cs
@@ -9,6 +9,7 @@ using IPLVotingSystem.Models;
 using System.Collections.Generic;
 using System.Linq;
 using System;
+//using System.Web.Mvc;
 
 namespace IPLVotingSystem.Areas.Admin.Controllers
 {
@@ -65,13 +66,23 @@ namespace IPLVotingSystem.Areas.Admin.Controllers
              ViewData["qlist"] = question;
 
            var option= _db.options.ToList();
-            ViewBag.olist = option;
+            ViewData["olist"] = option;
 
             return View();
         }
         [HttpGet]
-       public IActionResult EditQuestionary()
+       public IActionResult EditQuestionary(int qid)
         {
+            var quest = _db.questionaries.Where(q => q.qId == qid).SingleOrDefault();
+            var opt = _db.options.Where(x => x.qId == qid).SingleOrDefault();
+            ViewData["id"]= qid;
+            ViewBag.Question = quest.Question;
+            ViewBag.QuestionId = quest.qId;
+            ViewBag.Option1 = opt.option1;
+            ViewBag.Option2 = opt.option2;
+            ViewBag.Option3 = opt.option3;
+            ViewBag.Option4 = opt.option4;
+            
             return View();
         }
         [HttpPost]
@@ -82,25 +93,29 @@ namespace IPLVotingSystem.Areas.Admin.Controllers
             // var inputid = form["txtquestionid"].ToString();
           
             quest.Question = form["question"].ToString();
-            //_db.questionaries.Add(quest);
-            //_db.SaveChanges();
-            //fetch optionid
-            var resultq = _db.questionaries.OrderByDescending(x => x.qId).Take(1).SingleOrDefault();
-            option.qId = resultq.qId;
-            var result = _db.options.OrderByDescending(x => x.optionId).Take(1).SingleOrDefault();
-            var opid = result.optionId;
-            option.qId = Convert.ToInt32(form["txtquestionid"].ToString());
-            option.option1 = form["txtop1"].ToString(); option.option2 = form["txtop2"].ToString();
-            option.option3 = form["txtop3"].ToString(); option.option4 = form["txtop4"].ToString();
-            _db.options.Add(option);
-           
-            _db.SaveChanges();
+            quest.qId = Convert.ToInt32(form["txtquestionid"]);
+           // var newq = quest.Question; 
+            var resultq = _db.questionaries.OrderByDescending(x => x.qId == quest.qId).Take(1).SingleOrDefault();
+            resultq.Question = quest.Question;
+            //_db.questionaries.Add(newq);
+           // _db.SaveChanges();
 
-            int i = _db.SaveChanges();
-              if (i > 0)
-              {
-                  ViewBag.Msg = "Edited Suuceessfully.";
-              }
+            //fetch optionid
+
+
+
+            var result = _db.options.OrderByDescending(x => x.qId==resultq.qId).Take(1).SingleOrDefault();
+            //var opid = result.optionId;
+           // option.qId = Convert.ToInt32(form["txtquestionid"].ToString());
+            result.option1 = form["txtop1"].ToString(); result.option2 = form["txtop2"].ToString();
+            result.option3 = form["txtop3"].ToString(); result.option4 = form["txtop4"].ToString();
+            //_db.options.Add(option);
+           
+           int ans= _db.SaveChanges();
+            if(ans==1)
+            return RedirectToAction("ViewQuestionary");
+
+
             return View();
         }
            // return View();
@@ -111,11 +126,52 @@ namespace IPLVotingSystem.Areas.Admin.Controllers
         
         public IActionResult ViewResponce(Responces responces)
         {
-           // var responce=_db.responces.
+            // var responce=_db.responces.
+           
             return View();
+        }
+        [HttpGet]
+        public IActionResult DeletQuestionary(int qid)
+        {
+            var quest = _db.questionaries.Where(q => q.qId == qid).Take(1).SingleOrDefault();
+            var opt = _db.options.Where(x => x.qId == quest.qId).Take(1).SingleOrDefault();
+            ViewData["id"] = qid;
+            ViewBag.Question = quest.Question;
+            ViewBag.QuestionId = quest.qId;
+            ViewBag.Option1 = opt.option1;
+            ViewBag.Option2 = opt.option2;
+            ViewBag.Option3 = opt.option3;
+            ViewBag.Option4 = opt.option4;
+
+            return View();
+        }
+        [HttpPost]
+        [ActionName("DeletQuestionary")]
+        public IActionResult DeletQuestionaryCon(IFormCollection form)
+        {
+            int id = Convert.ToInt32(form["txtquestionid"]);
+            var delq = _db.questionaries.Where(q => q.qId == id ).SingleOrDefault();
+            var delo = _db.options.Where(o => o.qId == delq.qId).SingleOrDefault();
+
+            if(delq != null && delo != null)
+            {
+                _db.Remove(delo);
+                _db.Remove(delq);
+            }
+             int res=_db.SaveChanges();
+            if (res == 1)
+                ViewBag.Msg = "<script> alert'Record Deleted' </script>";
+            
+            return RedirectToAction("ViewQuestionary");
+
+           
+           // return View();
+
+
         }
         public IActionResult Feedback()
         {
+
             return View();
         }
     }
